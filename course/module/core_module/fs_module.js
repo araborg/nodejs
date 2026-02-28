3efbb03fbbce33480b7278434d691bcf56175d0a
araborg <arababs2015@gmail.com>
2026-02-28 18:33:02 +0100

    nodejs: 46

--- course/module/core_module/fs_module.js
+++ course/module/core_module/fs_module.js
@@ -3,7 +3,10 @@
 // fs = file system
 let fs = require("fs");
 
-console.log(fs);
+// console.log(fs);
 
 // srite file
-fs.writeFileSync("text.txt", "hi there");
+fs.writeFileSync("text.txt", "hi there\n");
+
+// to keep old text
+fs.appendFileSync("text.txt", "good morning");
