711391bc1dddc12b11250228fb26ff7721ed32e6
araborg <arababs2015@gmail.com>
2026-02-28 10:34:45 +0100

    nodejs: 34

--- course/module/core_module/path_module.js
+++ course/module/core_module/path_module.js
@@ -1,3 +1,11 @@
 let path = require("path");
 
-console.log(path);
+// console.log(path);
+// console.log(typeof path);
+
+let myPath =
+	"/home/babatunde/Documents/coding/nodejs/course/module/core_module/path_module.js";
+
+let parseParth = path.parse(myPath);
+
+console.log(parseParth);
