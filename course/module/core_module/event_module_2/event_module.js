e0218ffbceb44c40c24707b2f2ba15d3ef66f257
araborg <arababs2015@gmail.com>
2026-03-01 19:20:59 +0100

    nodejs: 66

---
+++ course/module/core_module/event_module_2/event_module.js
@@ -0,0 +1,13 @@
+// importing core module
+const events = require("events");
+
+const { EventEmitter } = events;
+
+const eventEmitter = new EventEmitter();
+
+// register an event with a param
+eventEmitter.on("event-1", function (obj) {
+	console.log("Hi there");
+
+	console.log(obj);
+});
