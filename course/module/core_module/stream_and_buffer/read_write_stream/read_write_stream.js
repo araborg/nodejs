f4786e4e164593b1082fd76bd50d2e961a2aa0d5
araborg <arababs2015@gmail.com>
2026-03-03 15:16:31 +0100

    nodejs: 97

---
+++ course/module/core_module/stream_and_buffer/read_write_stream/read_write_stream.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+const readStream = fs.createReadStream("./data.txt");
+const writeStream = fs.createWriteStream("output.txt");
+
+readStream.on("data", function (buffer) {
+	writeStream.write(buffer);
+});
