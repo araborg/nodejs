03c09bf09ba56b33c2affec5f568e1e8e58af834
araborg <arababs2015@gmail.com>
2026-03-03 09:24:39 +0100

    nodejs: 70

--- course/module/core_module/event_module_2/app.js
+++ course/module/core_module/event_module_2/app.js
@@ -1,15 +1,37 @@
 // importing core module
+
+// 1
+// const events = require("events");
+
+// const { EventEmitter } = events;
+
+// const eventEmitter = new EventEmitter();
+
+// // register an event with a param
+// eventEmitter.on("event-1", function (obj) {
+// 	console.log("Hi there");
+
+// 	console.log(obj);
+// });
+
+// // export eventEmitter
+// module.exports = eventEmitter;
+
+// 2
 const events = require("events");
+const fs = require("fs");
 
 const { EventEmitter } = events;
 
 const eventEmitter = new EventEmitter();
 
 // register an event with a param
-eventEmitter.on("event-1", function (obj) {
-	console.log("Hi there");
-
-	console.log(obj);
+eventEmitter.on("event-1", function (content) {
+	fs.write("test.txt", content, function (error) {
+		if (!error) {
+			console.log("File created successfully!");
+		}
+	});
 });
 
 // export eventEmitter
