c036443a782a0c17df9c5f1f44f34d209fa73a99
araborg <arababs2015@gmail.com>
2026-03-03 09:06:39 +0100

    nodejs: 69

--- course/module/core_module/event_module_2/index.js
+++ course/module/core_module/event_module_2/index.js
@@ -1,9 +1,13 @@
 // importing core module
-const events = require("events");
+// const events = require("events");
 
-const { EventEmitter } = events;
+// const { EventEmitter } = events;
 
-const eventEmitter = new EventEmitter();
+// const eventEmitter = new EventEmitter();
+
+const eventEmitter = require("./app");
 
 // emit or raise an event with param
 eventEmitter.emit("event-1", { msg: "by there", age: "28", name: "Julius" });
+
+// run node indes.js in the terminal
