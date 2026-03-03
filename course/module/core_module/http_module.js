17f39a87e634c87c61fc6009110d2360ec99a88c
araborg <arababs2015@gmail.com>
2026-03-03 09:34:37 +0100

    nodejs: 75

--- course/module/core_module/http_module.js
+++ course/module/core_module/http_module.js
@@ -4,6 +4,10 @@ const http = require("http");
 // cr8 a server
 const server = http.createServer();
 
+server.on("connection", function () {
+	console.log("New connection");
+});
+
 // listening to d server
 server.listen(3000, function () {
 	console.log("Server is starting on port: 3000");
