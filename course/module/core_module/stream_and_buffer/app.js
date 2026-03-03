f5b89027c88f3cd08f3af130e1c02956083db3db
araborg <arababs2015@gmail.com>
2026-03-03 14:28:06 +0100

    nodejs: 87

--- course/module/core_module/stream_and_buffer/app.js
+++ course/module/core_module/stream_and_buffer/app.js
@@ -1,7 +1,23 @@
+/*
+1.
+
 const fs = require("fs");
 
 const readStream = fs.createReadStream("./data.txt");
 
 readStream.on("data", function (buffer) {
 	console.log(buffer.toString());
+});
+
+
+2.
+
+*/
+
+const fs = require("fs");
+
+const readStream = fs.createReadStream("./data.txt", { encoding: "utf-8" });
+
+readStream.on("data", function (buffer) {
+	console.log(buffer);
 });
