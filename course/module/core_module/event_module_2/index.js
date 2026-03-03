4f365d40febab8baf755dfa6a81ef40ac5b8e324
araborg <arababs2015@gmail.com>
2026-03-03 09:25:41 +0100

    nodejs: 71

--- course/module/core_module/event_module_2/index.js
+++ course/module/core_module/event_module_2/index.js
@@ -8,6 +8,9 @@
 const eventEmitter = require("./app");
 
 // emit or raise an event with param
-eventEmitter.emit("event-1", { msg: "by there", age: "28", name: "Julius" });
+// eventEmitter.emit("event-1", { msg: "by there", age: "28", name: "Julius" });
 
 // run node indes.js in the terminal
+
+// 2.
+eventEmitter.emit("event-1");
