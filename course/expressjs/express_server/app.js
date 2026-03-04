5f75eb92cbeaf75f71deb5521a2124c155492857
araborg <arababs2015@gmail.com>
2026-03-04 22:20:52 +0100

    express: 12

--- course/expressjs/express_server/app.js
+++ course/expressjs/express_server/app.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", () => {});
+app.get("/", (req, res) => {
+	res.send("This is the home page");
+});
+
+app.listen(8000, () => {
+	console.log("Server is running on port: 8000");
+});
