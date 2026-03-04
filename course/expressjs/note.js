088baa938b1091f12fe91073b542f5adfe993464
araborg <arababs2015@gmail.com>
2026-03-04 17:16:05 +0100

    express: 3

--- course/expressjs/note.js
+++ course/expressjs/note.js
@@ -6,5 +6,8 @@ npm install express
 inside package.json file delete test inside d scripts obj
 and replace it with
 
-"scripts": "node ./index.js"
+"script1": "node ./index.js"
+
+dn type:
+npm run script1 in d ternminal
 */
