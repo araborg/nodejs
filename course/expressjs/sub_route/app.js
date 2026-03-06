f61ce31b0eab7239a43c40d0f8e3c21ebd0c6d9c
araborg <arababs2015@gmail.com>
2026-03-06 22:41:18 +0100

    nodejs: 27

--- course/expressjs/sub_route/app.js
+++ course/expressjs/sub_route/app.js
@@ -2,11 +2,20 @@ const express = require("express");
 
 const app = express();
 
+// route
 const admin = express.Router();
 const student = express.Router();
 
 app.use("/admin", admin);
 app.use("/student", student);
+
+admin.get("/home", (res, req, next) => {
+	res.send("Admin home route");
+});
+
+student.get("/home", (res, req, next) => {
+	res.send("Student home route");
+});
 
 // listen 4 d server
 app.listen(8000, () => {
