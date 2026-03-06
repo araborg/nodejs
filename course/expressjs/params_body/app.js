c2fe5bfa87198e68c4b4c90e134787c07e5c5adc
araborg <arababs2015@gmail.com>
2026-03-06 22:17:55 +0100

    nodejs: 11

--- course/expressjs/params_body/app.js
+++ course/expressjs/params_body/app.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const app = express();
 
+app.get("/users", (req, res) => {
+	res.send("Users list");
+});
+
+app.get("/users/:id", (req, res) => {
+	console.log(req.params);
+
+	res.send("User detail");
+});
+
 // listen 4 d server
 app.listen(8000, () => {
 	console.log("Server is running on port: 8000");
