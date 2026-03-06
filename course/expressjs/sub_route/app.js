57613aedb123439f9df698e6856630148a76efc4
araborg <arababs2015@gmail.com>
2026-03-06 22:38:29 +0100

    nodejs: 26

--- course/expressjs/sub_route/app.js
+++ course/expressjs/sub_route/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+const admin = express.Router();
+const student = express.Router();
+
+app.use("/admin", admin);
+app.use("/student", student);
+
 // listen 4 d server
 app.listen(8000, () => {
 	console.log("Server is running on port: 8000");
