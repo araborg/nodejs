8602edb5aa62be33199b7381f6238c0f1caef30f
araborg <arababs2015@gmail.com>
2026-03-06 22:14:43 +0100

    nodejs: 10

--- course/expressjs/params_body/app.js
+++ course/expressjs/params_body/app.js
@@ -2,9 +2,6 @@ const express = require("express");
 
 const app = express();
 
-// middleware
-app.use(express.json());
-
 // listen 4 d server
 app.listen(8000, () => {
 	console.log("Server is running on port: 8000");
