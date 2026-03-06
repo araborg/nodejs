d8fe040e25ba9a22f94afd321e519b3862888068
araborg <arababs2015@gmail.com>
2026-03-06 22:35:32 +0100

    nodejs: 24

---
+++ course/expressjs/sub_route/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/users", (req, res) => {
+	res.send("Users list");
+});
+
+app.get("/users/:userId", (req, res) => {
+	const { userId } = req.params;
+
+	// localhost:8000/users/2?query
+	// localhost:8000/users/2?name=babatunde&email=babatunde@gmail.com
+
+	console.log(req.query); // { name: 'babatunde', email: 'babatunde@gmail.com'}
+
+	const { name, email } = req.query;
+	console.log(name, email); // babatunde babatunde@gmail.com
+
+	res.send("User id : " + userId + " detail");
+});
+
+// listen 4 d server
+app.listen(8000, () => {
+	console.log("Server is running on port: 8000");
+});
+
+/* 
+    userId i.e params is a string and nt a number
+*/
