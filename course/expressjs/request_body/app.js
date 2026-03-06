ed3f35597d813eb05f5531d1aa6dc9af1f62535c
araborg <arababs2015@gmail.com>
2026-03-06 21:49:58 +0100

    nodejs: 4

--- course/expressjs/request_body/app.js
+++ course/expressjs/request_body/app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 
 app.post("/example", (req, res, next) => {
+	console.log(req);
+
+	const data = req.body;
+	console.log(data);
 	res.send("This is example response");
 });
 
@@ -10,3 +14,6 @@ app.post("/example", (req, res, next) => {
 app.listen(8000, () => {
 	console.log("Server is running on port: 8000");
 });
+
+// to cr8 body on postman:
+// click body --> raw--> json
