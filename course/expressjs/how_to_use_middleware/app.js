10b82743bc951ded117709be0da6c6f46d3f0f6b
araborg <arababs2015@gmail.com>
2026-03-07 14:54:03 +0100

    nodejs: 101

---
+++ course/expressjs/how_to_use_middleware/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/example", (req, res) => {
+	res.send("middleware");
+});
+
+// listen 4 d server
+app.listen(8000, () => {
+	console.log("Server is running on port: 8000");
+});
+
+/* 
+    
+
+
+*/
