52dae70d0275486cf8a818ee15e80c485babf4de
araborg <arababs2015@gmail.com>
2026-03-07 12:35:49 +0100

    nodejs: 80

---
+++ course/expressjs/response_format/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/example", (req, res, next) => {
+	// localhost:8000/home;
+	// res.send("<h1>Heading</h1>");
+
+	// res.render("pages/home.ejs");
+
+	// res.render("test.ejs");
+
+	res.render("test.ejs", { name: "babs", email: "test@gmail.com" });
+});
+
+// listen 4 d server
+app.listen(8000, () => {
+	console.log("Server is running on port: 8000");
+});
+
+/*
+    npm i ejs
+*/
