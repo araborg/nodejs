f0d665835a44048d5bc5f86ddcd7f62cbc586fd5
araborg <arababs2015@gmail.com>
2026-03-07 15:06:10 +0100

    nodejs: 107

--- course/expressjs/how_to_use_middleware/app.js
+++ course/expressjs/how_to_use_middleware/app.js
@@ -2,32 +2,25 @@ const express = require("express");
 
 const app = express();
 
+/*
 const middleware1 = (req, res, next) => {
 	console.log("middleware 1");
 
 	next();
 };
+
 const middleware2 = (req, res, next) => {
 	console.log("middleware 2");
 
 	res.send("Response from middleware 2");
 };
 
-// const middleware1 = (req, res, next) => {
-// 	console.log("middleware 1");
-
-// 	next();
-// };
-// const middleware2 = (req, res, next) => {
-// 	console.log("middleware 2");
-
-// 	next();
-// };
 const middleware3 = (req, res, next) => {
 	console.log("middleware 3");
 
 	next();
 };
+
 const middleware4 = (req, res, next) => {
 	console.log("middleware 4");
 
@@ -38,6 +31,55 @@ app.use(middleware1);
 app.use(middleware2);
 app.use(middleware3);
 app.use(middleware4);
+
+================================
+
+const middleware1 = (req, res, next) => {
+	console.log("middleware 1");
+
+	next();
+};
+
+const middleware2 = (req, res, next) => {
+	console.log("middleware 2");
+
+	next();
+};
+
+const middleware3 = (req, res, next) => {
+	console.log("middleware 3");
+
+	next();
+};
+
+const middleware4 = (req, res, next) => {
+	console.log("middleware 4");
+
+	next();
+};
+
+app.use(middleware1);
+app.use(middleware2);
+app.use(middleware3);
+app.use(middleware4);
+
+
+*/
+
+const middleware1 = (req, res, next) => {
+	console.log("middleware 1");
+
+	next();
+};
+
+const middleware2 = (req, res, next) => {
+	console.log("middleware 2");
+
+	next();
+};
+
+app.use(middleware1);
+app.use(middleware2);
 
 app.get("/example", (req, res) => {
 	res.send("Example route");
