0a7b55ea596c8f4f4d723bf8bf63fc68a773f34e
araborg <arababs2015@gmail.com>
2026-03-07 12:31:52 +0100

    nodejs: 76

--- course/expressjs/view_engline_and_html_response/app.js
+++ course/expressjs/view_engline_and_html_response/app.js
@@ -9,7 +9,8 @@ app.get("/example", (req, res, next) => {
 	// res.send("<h1>Heading</h1>");
 
 	// res.render("pages/home.ejs");
-	res.render("test.ejs");
+	// res.render("test.ejs");
+	res.render("test.ejs", { name: "babs", email: "test@gmail.com" });
 });
 
 // listen 4 d server
