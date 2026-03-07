921a1d319503db9dc9db48f057015d8b27472992
araborg <arababs2015@gmail.com>
2026-03-07 15:54:19 +0100

    nodejs: 124

--- course/expressjs/error_handling/app.js
+++ course/expressjs/error_handling/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/example", (req, res) => {
-	res.send("middleware");
+	res.send("Error handling");
 });
 
 // listen 4 d server
@@ -12,34 +12,8 @@ app.listen(8000, () => {
 });
 
 /* 
-    Middleware is a function in wc we access req &
-    res obj. 
-
-    A middleware function can do three types of tasks:
-    (assuming we have 4 middlewares)
-
-    1. It can call next function to handover request 
-    to next middleware function.
-
-    2. It can direct response back to client, for 
-    example middleware two direct back response to 
-    client hence request will not pass to middleware 
-    three and middleware four.
-
-    3. Middleware can throw an error and error catching 
-    middleware can catch this error.
-
-
-    Types of middleware:
-    1. App levl middleware
-
-    2. Route level middleware
-
-    3. Error handling middleware
-
-    4. Built-in middleware
-
-    5. Third-party middleware
+    Error Handling:
+    
 
 
 */
