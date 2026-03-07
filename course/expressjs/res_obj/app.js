1742d650fd80f2c74d654193df7f52598268b993
araborg <arababs2015@gmail.com>
2026-03-07 07:36:01 +0100

    nodejs: 59

--- course/expressjs/res_obj/app.js
+++ course/expressjs/res_obj/app.js
@@ -2,19 +2,19 @@ const express = require("express");
 
 const app = express();
 
+app.get("/test", (res, req) => {
+	res.setEncoding("Test response");
+});
+
 app.get("/example", (req, res) => {
-	console.log(req.hostname); // localhost
-	console.log(req.ip); // 127.0.0.1
-	console.log(req.method); // GET
-	console.log(req.protocol); // http
-	console.log(req.secure); // false
+	// res.end()
 
-	// check under header tab on postman
-	console.log(req.accepts()); // [ '*/*' ]
-	console.log(req.get("Content-Type")); // application/json
-	console.log(req.get("Accept")); // [ '*/*' ] || application/json
+	// res.send("Example route");
 
-	res.send("Example route");
+	res.json({
+		name: "Babatunde",
+		email: "babs@gmail.com",
+	});
 });
 
 // listen 4 d server
