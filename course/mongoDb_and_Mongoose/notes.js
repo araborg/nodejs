787a8365d6cb8251cd5739eb31b43e38b9f16c06
araborg <arababs2015@gmail.com>
2026-03-07 16:59:57 +0100

    nodejs: 131

--- course/mongoDb_and_Mongoose/notes.js
+++ course/mongoDb_and_Mongoose/notes.js
@@ -1,5 +1,10 @@
 /*
 A database is a storage where we store data.
 
+ database is
+
+a secure, persistent optimized storage where we can save data, update data, delete data or page,
+
+or get data in a optimized and secured way.
 
 */
