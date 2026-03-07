0e7fdb72cf4baf46e5a3a36fda7cb11c34ef970d
araborg <arababs2015@gmail.com>
2026-03-07 16:50:33 +0100

    nodejs: 126

--- course/expressjs/error_handling/app.js
+++ course/expressjs/error_handling/app.js
@@ -3,10 +3,20 @@ const express = require("express");
 const app = express();
 
 app.get("/example", (req, res) => {
-	throw new Error("Test error");
+	// throw new Error("Test error");
+
+	console.log(xyz);
 
 	res.send("Error handling");
 });
+
+const errorMiddleware = (error, req, res, next) => {
+	console.log(error);
+
+	res.send("Custom error");
+};
+
+app.use(errorMiddleware);
 
 // listen 4 d server
 app.listen(8000, () => {
