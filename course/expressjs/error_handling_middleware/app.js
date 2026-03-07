43f79e7e9a6f3a154f4c0d7d99ac74df2f8a49d3
araborg <arababs2015@gmail.com>
2026-03-07 15:50:19 +0100

    nodejs: 122

--- course/expressjs/error_handling_middleware/app.js
+++ course/expressjs/error_handling_middleware/app.js
@@ -5,8 +5,9 @@ const app = express();
 
 // built-in middleware
 app.use(express.json());
-app.use(cookieParser);
+app.use(cookieParser());
 
+// error handling middleware
 const middleware1 = (req, res, next) => {
 	// throw new Error("Error from middleware 1");
 
