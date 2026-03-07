750d898234ad986ebf723849ff9d21f830a1ada7
araborg <arababs2015@gmail.com>
2026-03-07 17:02:03 +0100

    nodejs: 132

--- course/mongoDb_and_Mongoose/notes.js
+++ course/mongoDb_and_Mongoose/notes.js
@@ -1,10 +1,14 @@
 /*
 A database is a storage where we store data.
 
- database is
-
-a secure, persistent optimized storage where we can save data, update data, delete data or page,
+Database is a secure, persistent optimized storage 
+where we can save data, update data, delete data or page,
 
 or get data in a optimized and secured way.
+
+There are mainly two types of database:
+1. Relational database management system (RDBMS)
+2. Non relational database.
+
 
 */
