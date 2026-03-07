be7193eea8d72dad9edec9cb9e6d352a5bea37cb
araborg <arababs2015@gmail.com>
2026-03-07 14:26:01 +0100

    nodejs: 88

---
+++ course/expressjs/middleware/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/users", (req, res) => {
+	res.send("Users list");
+});
+
+app.get("/users/:userId", (req, res) => {
+	// http://localhost:8000/users/2
+	console.log(req.params); // { userId: '2' }
+
+	// const { userId } = req.params;
+
+	res.send("User detail");
+});
+
+// listen 4 d server
+app.listen(8000, () => {
+	console.log("Server is running on port: 8000");
+});
+
+/* 
+    userId i.e params is a string and nt a number
+*/
