9788f0b7176a11a419f2cf92fa07d4ddb741e320
araborg <arababs2015@gmail.com>
2026-03-08 08:28:40 +0100

    DB: 2

--- course/mongoDb_and_Mongoose/establish_db_connectn/app.js
+++ course/mongoDb_and_Mongoose/establish_db_connectn/app.js
@@ -10,10 +10,6 @@ const errorMiddleware = (error, req, res, next) => {
 	res.status(500).send(error.message);
 };
 
-app.get("/example", (req, res) => {
-	res.send("Example route");
-});
-
 app.use(errorMiddleware);
 
 // listen 4 d server
@@ -22,9 +18,7 @@ app.listen(8000, () => {
 });
 
 /* 
-    Error handling middleware
-
-    npm i cookie-parser
+    Dababase Connection:
 
 
 */
