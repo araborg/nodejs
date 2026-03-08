2581f6698225d004f1fb9220e6db949f1ca6f867
araborg <arababs2015@gmail.com>
2026-03-08 14:35:52 +0100

    DB: 11

---
+++ course/mongoDb_and_Mongoose/add_multiple_documt/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const app = express();
+
+const connectionUrl = "mongodb://localhost:27017";
+
+const client = new mongodb.MongoClient(connectionUrl);
+
+client
+	.connect()
+	.then(() => console.log("Database connection successful"))
+	.catch((error) => console.log(error));
+
+const errorMiddleware = (error, req, res, next) => {
+	res.status(500).send(error.message);
+};
+
+app.use(errorMiddleware);
+
+// listen 4 d server
+app.listen(8000, () => {
+	console.log("Server is running on port: 8000");
+});
+
+/* 
+    Dababase Connection:
+
+    Install mongodb: 
+    npm i mongodb
+    
+    start d mongo db in terminal:
+    sudo systemctl start mongod
+
+    sudo systemctl status mongod
+
+    mongodb://localhost:27017
+*/
---
+++ course/mongoDb_and_Mongoose/add_single_documt/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const app = express();
+
+const connectionUrl = "mongodb://localhost:27017";
+
+const client = new mongodb.MongoClient(connectionUrl);
+
+client
+	.connect()
+	.then(() => console.log("Database connection successful"))
+	.catch((error) => console.log(error));
+
+const db = client.db("schoolDB");
+
+const student = db.collection("student");
+
+app.post("/student", (req, res, next) => {
+	student.insertOne();
+});
+
+// listen 4 d server
+app.listen(8000, () => {
+	console.log("Server is running on port: 8000");
+});
+
+/* 
+    Dababase Connection:
+
+    Install mongodb: 
+    npm i mongodb
+    
+    start d mongo db in terminal:
+    sudo systemctl start mongod
+
+    sudo systemctl status mongod
+
+    mongodb://localhost:27017
+*/
