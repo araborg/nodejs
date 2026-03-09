6aafa8f9f154bb0e75f18dab3a287c8a480bd5c5
araborg <arababs2015@gmail.com>
2026-03-09 23:20:09 +0100

    node: 56

---
+++ course/ODM/schema/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// ds is import to receive json data 4rm postman
+app.use(express.json());
+
+// connect to db: note d  diff
+const connectionUrl = "mongodb://localhost:27017/schoolDb";
+
+mongoose
+	.connect(connectionUrl)
+	.then(() => console.log("Database connection successful"))
+	.catch((error) => console.log(error));
+
+const errorMiddleware = (error, req, res, next) => {
+	res.status(500).send((err) => err.message);
+};
+
+app.use(errorMiddleware);
+
+// listen 4 d server
+app.listen(8000, () => {
+	console.log("Server is running on port: 8000");
+});
+
+/* 
+    Mongoose
+        
+    What to install:
+    npm init
+
+    nodemon:
+    npm i nodemon
+
+    express:
+    npm i express
+
+    mongodb:
+    npm i mongodb
+
+    mongoose:
+    npm i mongoose
+
+*/
