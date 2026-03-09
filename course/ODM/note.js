ba1721a95da1a80501c6dca18e15b2c49f92d082
araborg <arababs2015@gmail.com>
2026-03-09 22:33:57 +0100

    node: 39

---
+++ course/ODM/note.js
@@ -0,0 +1,36 @@
+/*
+Wthat is ODM?
+ODM: Object Data Modeling. It is a db concept
+
+Here O stands for object and this object means 
+JavaScript object.
+
+It is much more easier if we can think of 
+everything as object in JavaScript.
+
+NodejsApp ------------------> mongoose 
+    |                             |
+    | Object                      |
+    | mapping                     |
+    |                             |
+    V                             V
+mongodb <----------------------mongoDriver
+
+
+Benefits of mongoose:
+1. Abstraction from low level MongoDB driver
+
+2. Relationship between NoSQL data
+
+3. Provide schema validation
+
+4. Object data mapping
+
+5. 40% less code
+
+
+
+
+
+
+*/
