c9666231b046d8da37a8500092b4b8afa8edec9c
araborg <arababs2015@gmail.com>
2026-03-11 19:56:15 +0100

    node: 4

--- course/ODM/crud/update_single_doc/index.js
+++ course/ODM/crud/update_single_doc/index.js
@@ -68,7 +68,7 @@ app.put("/student/single", async (req, res, next) => {
 
 		await Student.findOneAndUpdate({ email: email }, { dept: dept }); // 2 objs
 
-		res.send(200).json({ message: "Student updated successfully" });
+		res.status(200).json({ message: "Student updated successfully" });
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
