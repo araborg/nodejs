a386909f06bbcc2d75997b693085b539c55de1f0
araborg <arababs2015@gmail.com>
2026-03-11 09:10:22 +0100

    mongoose: 11

--- course/ODM/crud/add_multiple_doc/index.js
+++ course/ODM/crud/add_multiple_doc/index.js
@@ -14,6 +14,7 @@ mongoose
 	.then(() => console.log("Database connection successful"))
 	.catch((error) => console.log(error));
 
+// Schema
 const studentSchema = mongoose.Schema({
 	name: String,
 	email: String,
