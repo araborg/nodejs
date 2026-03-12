7cb51048fb532e7b4544486cea30def2fb6e4d3f
araborg <arababs2015@gmail.com>
2026-03-12 19:45:30 +0100

    crud: 2

--- course/ODM/crud/find_single_doc/index.js
+++ course/ODM/crud/find_single_doc/index.js
@@ -168,6 +168,10 @@ app.get("/student/single/:studentId", async (req, res, next) => {
 	}
 });
 
+// get multiple student
+
+// get multiple student using
+
 const errorMiddleware = (error, req, res, next) => {
 	res.status(500).send((err) => err.message);
 };
