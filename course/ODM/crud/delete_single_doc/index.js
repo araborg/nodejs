5f2e0877cd2fc23e42706316b44db733c0517d45
araborg <arababs2015@gmail.com>
2026-03-12 21:10:25 +0100

    crud: 27

--- course/ODM/crud/delete_single_doc/index.js
+++ course/ODM/crud/delete_single_doc/index.js
@@ -238,6 +238,7 @@ app.delete("/student/multiple", async (req, res, next) => {
 
 		const { dept } = req.query;
 
+		// ds deletes all d student with d dept passed as query
 		await Student.deleteMany({ dept: dept });
 
 		res.status(200).json({
