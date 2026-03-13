6717fdad0e6ad62af59d96220d04eea2e5eba495
araborg <arababs2015@gmail.com>
2026-03-13 05:56:37 +0100

    update: 4

---
+++ mongodb_and_mongoose_methods.js
@@ -0,0 +1,99 @@
+/*
+Add/Cr8 a doc:
+mongodb methods:
+
+
+
+mongoose methods:
+
+
+
+
+====================================
+
+Add/Cr8 multiple docs:
+mongodb methods:
+
+
+
+mongoose methods:
+
+
+
+
+====================================
+
+Read a doc:
+mongodb methods:
+
+
+
+mongoose methods:
+
+
+
+
+====================================
+
+Read multiple doc:
+mongodb methods:
+
+
+
+mongoose methods:
+
+
+
+
+====================================
+
+Update a doc:
+mongodb methods:
+
+
+
+mongoose methods:
+
+
+
+
+Update multiple docs:
+mongodb methods:
+
+
+
+mongoose methods:
+
+
+
+
+====================================
+
+Delete a doc:
+mongodb methods:
+
+
+
+mongoose methods:
+
+
+
+
+====================================
+
+Delete multiple docs:
+mongodb methods:
+
+
+
+mongoose methods:
+
+
+
+
+====================================
+
+
+
+
+*/
