c8ba871ce6fac5a061511d87148e48e31ee12722
araborg <arababs2015@gmail.com>
2026-03-13 06:59:47 +0100

    update: 16

--- course/mongoDb_and_Mongoose/find_single_documt/app.js
+++ course/mongoDb_and_Mongoose/find_single_documt/app.js
@@ -3,9 +3,6 @@ const mongodb = require("mongodb");
 
 const app = express();
 
-// ds is import to receive json data 4rm postman
-app.use(express.json());
-
 // connect to db
 const connectionUrl = "mongodb://localhost:27017";
 
@@ -34,7 +31,8 @@ app.post("/student", (req, res, next) => {
 });
 
 // find one student
-app.get("/student/:id", (req, res, next) => {
+// app.get("/student/:id", (req, res, next) => {
+app.get("/student", (req, res, next) => {
 	// http://localhost:8000/student
 
 	// student
@@ -46,21 +44,23 @@ app.get("/student/:id", (req, res, next) => {
 	// using postman url
 	// http://localhost:8000/student?email=john1@gmail.com
 
-	// const { email } = req.query; // req.query & not req.body
-	const { id } = req.params; // req.query & not req.body
+	const { email } = req.query; // req.query & not req.body
+	console.log(email);
+	// const { id } = req.params; // req.query & not req.body
+	// console.log(id);
 	// console.log(email);
 
 	student
 		// .findOne({ name: "John Doe2" })
-		// .findOne({ email: email })
-		.findById({ email: email })
+		// .findById({ _id: id })
+		.findOne({ email: email })
 		.then((data) => res.status(200).json(data)) // notice we use data here
 		.catch((err) => res.status(500).send(err.message));
 });
 
 // listen 4 d server
 app.listen(8000, () => {
-	console.log("Server is running on port: 8000");
+	console.log("Server is running on port: 7000");
 });
 
 /* 
