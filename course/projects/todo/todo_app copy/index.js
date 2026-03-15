b0691f6c47e7bb1fc5a80551231204d532f7bcdd
araborg <arababs2015@gmail.com>
2026-03-15 16:31:18 +0100

    todo app: 9

---
+++ course/projects/todo/todo_app copy/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const PORT = 8000;
+
+// init app
+const app = express();
+
+// view engine
+app.set("view engine", "ejs");
+
+// listen to server
+app.listen(PORT, () => {
+	console.log(`Server is running on port: ${PORT}`);
+});
