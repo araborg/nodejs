6102b9a73aba327fdc7c3a80cdc123fe08364e29
araborg <arababs2015@gmail.com>
2026-03-15 17:45:30 +0100

    todo app: 32

--- course/projects/todo/todo_app/index.js
+++ course/projects/todo/todo_app/index.js
@@ -42,6 +42,14 @@ app.get("/update-todo", (req, res, next) => {
 	}
 });
 
+app.get("/delete-todo", (req, res, next) => {
+	try {
+		res.render("deleteTodo");
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
 // listen to server
 app.listen(PORT, () => {
 	console.log(`Server is running on port: ${PORT}`);
