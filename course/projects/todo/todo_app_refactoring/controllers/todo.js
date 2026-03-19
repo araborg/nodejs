5a988a50d67b39482c540761c3c51440ea8d470d
araborg <arababs2015@gmail.com>
2026-03-19 20:11:41 +0100

    todo app: 17

--- course/projects/todo/todo_app_refactoring/controllers/todo.js
+++ course/projects/todo/todo_app_refactoring/controllers/todo.js
@@ -13,4 +13,52 @@ const homeController = async (req, res, next) => {
 	}
 };
 
-module.exports = { homeController };
+const addTodoFormController = (req, res, next) => {
+	try {
+		res.render("newTodo", { title: "New todo" });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+const updateTodoFormController = (req, res, next) => {
+	try {
+		res.render("updateTodo", { title: "Update todo" });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+const deleteTodoPageController = (req, res, next) => {
+	try {
+		res.render("deleteTodo", { title: "Delete todo" });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+const addTodoController = async (req, res, next) => {
+	try {
+		const { title, desc } = req.body;
+
+		if (!title) {
+			return res.status(400).json({ message: "Title is required" });
+		}
+
+		const newTodo = new Todo({ title, desc });
+
+		await newTodo.save();
+
+		res.redirect("/");
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+module.exports = {
+	homeController,
+	addTodoFormController,
+	updateTodoFormController,
+	deleteTodoPageController,
+	addTodoController,
+};
--- course/projects/todo/todo_app_refactoring/routes/todo.js
+++ course/projects/todo/todo_app_refactoring/routes/todo.js
@@ -2,54 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
-const Todo = require("../models/Todo");
-
 const todo = require("../controllers/todo");
 
 // pages
 router.get("/", todo.homeController);
 
-router.get("/add-todo", (req, res, next) => {
-	try {
-		res.render("newTodo", { title: "New todo" });
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
-});
+router.get("/add-todo", todo.addTodoController);
 
-router.get("/update-todo", (req, res, next) => {
-	try {
-		res.render("updateTodo", { title: "Update todo" });
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
-});
+router.get("/update-todo", todo.updateTodoFormController);
 
-router.get("/delete-todo", (req, res, next) => {
-	try {
-		res.render("deleteTodo", { title: "Delete todo" });
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
-});
+router.get("/delete-todo", todo.deleteTodoPageController);
 
 // routes
-router.post("/add-todo", async (req, res, next) => {
-	try {
-		const { title, desc } = req.body;
-
-		if (!title) {
-			return res.status(400).json({ message: "Title is required" });
-		}
-
-		const newTodo = new Todo({ title, desc });
-
-		await newTodo.save();
-
-		res.redirect("/");
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
-});
+router.post("/add-todo", todo.addTodoController);
 
 module.exports = router;
