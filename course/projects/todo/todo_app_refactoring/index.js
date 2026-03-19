9948070b5a49498c275fd6d699cba87569727d58
araborg <arababs2015@gmail.com>
2026-03-19 19:40:23 +0100

    todo app: 8

--- course/projects/todo/todo_app_refactoring/index.js
+++ course/projects/todo/todo_app_refactoring/index.js
@@ -50,61 +50,61 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public"))); // app.use()
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// pages
-app.get("/", async (req, res, next) => {
-	try {
-		const todos = await Todo.find({}).sort({ createdAt: -1 });
+// // pages
+// app.get("/", async (req, res, next) => {
+// 	try {
+// 		const todos = await Todo.find({}).sort({ createdAt: -1 });
 
-		res.locals.moment = moment;
+// 		res.locals.moment = moment;
 
-		res.render("index", { title: "List todo", todos: todos });
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
-});
+// 		res.render("index", { title: "List todo", todos: todos });
+// 	} catch (error) {
+// 		res.status(500).json({ message: error.message });
+// 	}
+// });
 
-app.get("/add-todo", (req, res, next) => {
-	try {
-		res.render("newTodo", { title: "New todo" });
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
-});
+// app.get("/add-todo", (req, res, next) => {
+// 	try {
+// 		res.render("newTodo", { title: "New todo" });
+// 	} catch (error) {
+// 		res.status(500).json({ message: error.message });
+// 	}
+// });
 
-app.get("/update-todo", (req, res, next) => {
-	try {
-		res.render("updateTodo", { title: "Update todo" });
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
-});
+// app.get("/update-todo", (req, res, next) => {
+// 	try {
+// 		res.render("updateTodo", { title: "Update todo" });
+// 	} catch (error) {
+// 		res.status(500).json({ message: error.message });
+// 	}
+// });
 
-app.get("/delete-todo", (req, res, next) => {
-	try {
-		res.render("deleteTodo", { title: "Delete todo" });
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
-});
+// app.get("/delete-todo", (req, res, next) => {
+// 	try {
+// 		res.render("deleteTodo", { title: "Delete todo" });
+// 	} catch (error) {
+// 		res.status(500).json({ message: error.message });
+// 	}
+// });
 
-// routes
-app.post("/add-todo", async (req, res, next) => {
-	try {
-		const { title, desc } = req.body;
+// // routes
+// app.post("/add-todo", async (req, res, next) => {
+// 	try {
+// 		const { title, desc } = req.body;
 
-		if (!title) {
-			return res.status(400).json({ message: "Title is required" });
-		}
+// 		if (!title) {
+// 			return res.status(400).json({ message: "Title is required" });
+// 		}
 
-		const newTodo = new Todo({ title, desc });
+// 		const newTodo = new Todo({ title, desc });
 
-		await newTodo.save();
+// 		await newTodo.save();
 
-		res.redirect("/");
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
-});
+// 		res.redirect("/");
+// 	} catch (error) {
+// 		res.status(500).json({ message: error.message });
+// 	}
+// });
 
 // listen to server
 app.listen(PORT, () => {
---
+++ course/projects/todo/todo_app_refactoring/routes/todo.js
@@ -0,0 +1,55 @@
+// pages
+app.get("/", async (req, res, next) => {
+	try {
+		const todos = await Todo.find({}).sort({ createdAt: -1 });
+
+		res.locals.moment = moment;
+
+		res.render("index", { title: "List todo", todos: todos });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+app.get("/add-todo", (req, res, next) => {
+	try {
+		res.render("newTodo", { title: "New todo" });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+app.get("/update-todo", (req, res, next) => {
+	try {
+		res.render("updateTodo", { title: "Update todo" });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+app.get("/delete-todo", (req, res, next) => {
+	try {
+		res.render("deleteTodo", { title: "Delete todo" });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+// routes
+app.post("/add-todo", async (req, res, next) => {
+	try {
+		const { title, desc } = req.body;
+
+		if (!title) {
+			return res.status(400).json({ message: "Title is required" });
+		}
+
+		const newTodo = new Todo({ title, desc });
+
+		await newTodo.save();
+
+		res.redirect("/");
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
