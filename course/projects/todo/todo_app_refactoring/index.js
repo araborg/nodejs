7f575521ac5fb7a8687be18d3c892dd4adad90ad
araborg <arababs2015@gmail.com>
2026-03-19 19:49:04 +0100

    todo app: 13

--- course/projects/todo/todo_app_refactoring/index.js
+++ course/projects/todo/todo_app_refactoring/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-// const mongoose = require("mongoose");
 const path = require("path");
 
 const bodyParser = require("body-parser");
@@ -7,8 +6,6 @@ const bodyParser = require("body-parser");
 const PORT = 8000;
 
 const connectMongodb = require("./init/mongodb");
-
-const Todo = require("./models/Todo");
 
 const todoRoute = require("./routes/todo");
 
@@ -18,92 +15,11 @@ const app = express();
 // mongodb connection
 connectMongodb();
 
-// mongoose
-// 	.connect(connectionUrl)
-// 	.then(() => console.log("Database connection successfull"))
-// 	.catch((err) => console.log(err.message));
-
-// Schema
-// const todoSchema = mongoose.Schema(
-// 	{
-// 		// title: String,
-
-// 		// title: { type: String, required: true, unique: true, kMaxLength: 20, minLenth: 5, trim: true,},
-
-// 		title: {
-// 			type: String,
-// 			required: true,
-// 		},
-
-// 		desc: String,
-// 	},
-
-// 	{ timestamps: true },
-// );
-
-// const Todo = mongoose.model("todoApp", todoSchema);
-
 // view engine
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public"))); // app.use()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", todoRoute);
-
-// // pages
-// app.get("/", async (req, res, next) => {
-// 	try {
-// 		const todos = await Todo.find({}).sort({ createdAt: -1 });
-
-// 		res.locals.moment = moment;
-
-// 		res.render("index", { title: "List todo", todos: todos });
-// 	} catch (error) {
-// 		res.status(500).json({ message: error.message });
-// 	}
-// });
-
-// app.get("/add-todo", (req, res, next) => {
-// 	try {
-// 		res.render("newTodo", { title: "New todo" });
-// 	} catch (error) {
-// 		res.status(500).json({ message: error.message });
-// 	}
-// });
-
-// app.get("/update-todo", (req, res, next) => {
-// 	try {
-// 		res.render("updateTodo", { title: "Update todo" });
-// 	} catch (error) {
-// 		res.status(500).json({ message: error.message });
-// 	}
-// });
-
-// app.get("/delete-todo", (req, res, next) => {
-// 	try {
-// 		res.render("deleteTodo", { title: "Delete todo" });
-// 	} catch (error) {
-// 		res.status(500).json({ message: error.message });
-// 	}
-// });
-
-// // routes
-// app.post("/add-todo", async (req, res, next) => {
-// 	try {
-// 		const { title, desc } = req.body;
-
-// 		if (!title) {
-// 			return res.status(400).json({ message: "Title is required" });
-// 		}
-
-// 		const newTodo = new Todo({ title, desc });
-
-// 		await newTodo.save();
-
-// 		res.redirect("/");
-// 	} catch (error) {
-// 		res.status(500).json({ message: error.message });
-// 	}
-// });
 
 // listen to server
 app.listen(PORT, () => {
