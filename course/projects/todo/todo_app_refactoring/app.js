c755681581330ee28c16340f5575ac1ef6a714bf
araborg <arababs2015@gmail.com>
2026-03-19 20:16:52 +0100

    todo app: 20

--- course/projects/todo/todo_app_refactoring/app.js
+++ course/projects/todo/todo_app_refactoring/app.js
@@ -17,3 +17,5 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public"))); // app.use()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", todoRoute);
+
+module.exports = app;
--- course/projects/todo/todo_app_refactoring/index.js
+++ course/projects/todo/todo_app_refactoring/index.js
@@ -1,24 +1,6 @@
-const express = require("express");
-const path = require("path");
-
-const bodyParser = require("body-parser");
-
-const connectMongodb = require("./init/mongodb");
-const todoRoute = require("./routes/todo");
+const app = require("./app");
 
 const PORT = 8000;
-
-// init app
-const app = express();
-
-// mongodb connection
-connectMongodb();
-
-// view engine
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public"))); // app.use()
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/", todoRoute);
 
 // listen to server
 app.listen(PORT, () => {
