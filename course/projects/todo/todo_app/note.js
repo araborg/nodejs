303f2cb2c758745fcc2c6685f29d36d83c3a1df3
araborg <arababs2015@gmail.com>
2026-03-19 15:32:51 +0100

    todo project: 6

--- course/projects/todo/todo_app/note.js
+++ course/projects/todo/todo_app/note.js
@@ -3,7 +3,12 @@ npm init
 
 npm i express mongoose ejs nodemon
 
+body-parser:
+
 npm i body-parser
+
+Parse incoming request bodies in a middleware before 
+your handlers, available under the req.body property.
 
 
 
