edfc867095f7481ac20e5c89fa5307c70bf85049
araborg <arababs2015@gmail.com>
2026-03-20 14:54:59 +0100

    todo: 24

--- course/projects/todo/todo_app_refactoring/routes/todo.js
+++ course/projects/todo/todo_app_refactoring/routes/todo.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const todo = require("../controllers/todo");
 
-// pages
+// gets
 router.get("/", todo.homeController);
 
 router.get("/add-todo", todo.addTodoFormController);
@@ -13,11 +13,12 @@ router.get("/update-todo", todo.updateTodoFormController);
 
 router.get("/delete-todo", todo.deleteTodoPageController);
 
-// routes
+// posts
 router.post("/add-todo", todo.addTodoController);
 
 router.post("/update-todo/:id", todo.updateTodoController);
 
+// get
 router.get("/confirm-delete", todo.deleteTodoController);
 
 module.exports = router;
