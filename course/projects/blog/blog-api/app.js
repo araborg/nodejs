4416bc3aec9aed1fabaa5a9567b48e88bb3e284a
araborg <arababs2015@gmail.com>
2026-03-21 16:17:55 +0100

    blog_api: 4

---
+++ course/projects/blog/blog-api/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// init app
+const app = express();
+
+// 3rd party middleware
+app.use(express.json({ limit: "500mb" }));
+app.use(bodyParser.urlencoded({ limit: "500mb" }));
