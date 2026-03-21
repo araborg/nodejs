2ed986ce6d38955b0000bf80d002c64d718a6d3b
araborg <arababs2015@gmail.com>
2026-03-21 16:25:46 +0100

    blog_api: 6

--- course/projects/blog/blog-api/index.js
+++ course/projects/blog/blog-api/index.js
@@ -1,2 +1,8 @@
 const http = require("http");
 const app = require("./app");
+const { port } = require("./config/keys");
+
+// cr8 server instance
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Server is running on port: ${port}`));
