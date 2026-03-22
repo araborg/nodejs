7ff36d0aeb58e60d5f63a819f0044b3271102154
araborg <arababs2015@gmail.com>
2026-03-22 13:56:51 +0100

    blog api: 5

--- course/projects/blog/blog-api/routes/auth.js
+++ course/projects/blog/blog-api/routes/auth.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
+
+// controllers
 const { authController } = require("../controllers");
 
+// routes
 router.post("/signup", authController);
 
 module.exports = router;
