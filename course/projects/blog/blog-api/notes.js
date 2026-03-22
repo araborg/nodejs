61142f9ef20735c01e19b223e88df61ac3161e9c
araborg <arababs2015@gmail.com>
2026-03-22 14:46:50 +0100

    blog api: 13

--- course/projects/blog/blog-api/notes.js
+++ course/projects/blog/blog-api/notes.js
@@ -6,7 +6,10 @@ npm i express nodemon mongoose dotenv body-parser
 go to: https://www.mongodb.com/
 
 
+This helps console log our statements:
 
+npm i morgan
+HTTP request logger middleware for node.js
 
 
 
