9bc44eb36889325950f7da88dea5b2c86fabab04
araborg <arababs2015@gmail.com>
2026-03-22 14:51:53 +0100

    blog api: 15

--- course/projects/blog/blog-api/notes.js
+++ course/projects/blog/blog-api/notes.js
@@ -11,7 +11,10 @@ This helps console log our statements:
 npm i morgan
 HTTP request logger middleware for node.js
 
+After it's been included, your terminal shd show smth
+like this: 
 
+POST /api/v1/auth/signup 201 146.581 ms - 42
 
 
 
