a927207d405e574c7475a66dc4a2e3b87afbf7e3
araborg <arababs2015@gmail.com>
2026-03-22 14:08:06 +0100

    blog api: 9

--- course/projects/blog/blog-api/controllers/auth.js
+++ course/projects/blog/blog-api/controllers/auth.js
@@ -1,3 +1,19 @@
-const signup = async (req, resizeBy, next) => {};
+const { User } = require("../models");
+
+const signup = async (req, resizeBy, next) => {
+	try {
+		const { name, email, password, role } = req.body;
+
+		const newUser = new User({ name, email, password, role });
+
+		await newUser.save();
+
+		res.status(201).json({ message: "User registered successfully" });
+	} catch (error) {
+		// console.log(error.message);
+
+		next(error);
+	}
+};
 
 module.exports = { signup };
