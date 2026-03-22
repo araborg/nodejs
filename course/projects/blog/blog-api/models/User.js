83126d72935e34e9c2e53b23d7ad632306eb732e
araborg <arababs2015@gmail.com>
2026-03-22 15:03:35 +0100

    blog api: 17

--- course/projects/blog/blog-api/models/User.js
+++ course/projects/blog/blog-api/models/User.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema(
 	{
 		name: { type: String, required: true },
+
 		email: { type: String, required: true, unique: true, trim: true },
+
 		password: { type: String, required: true, minLength: 6 },
 
 		// role 1: super admin, role 2: normal admin, role 3: normal user
