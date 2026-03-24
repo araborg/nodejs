35935ffe32d19f68c255d1e18e0305c36cd68f39
araborg <arababs2015@gmail.com>
2026-03-24 21:28:51 +0100

    blog_api: 25

--- course/projects/blog/blog-api/utils/hashpassword.js
+++ course/projects/blog/blog-api/utils/hashpassword.js
@@ -1,5 +1,13 @@
 const bcrypt = require("bcryptjs");
 
-const hashPassword = (password) => {};
+const hashPassword = (password) => {
+	return new Promise((resolve, reject) => {
+		bcrypt.genSalt(12, (error, salt) => {
+			if (error) {
+				return reject(error);
+			}
+		});
+	});
+};
 
 module.exports = hashPassword;
