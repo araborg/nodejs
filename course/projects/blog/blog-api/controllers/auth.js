c69aad7801b4ed57e9680097e8cbc11b2f87ea8c
araborg <arababs2015@gmail.com>
2026-03-24 08:04:23 +0100

    blog_api: 7

--- course/projects/blog/blog-api/controllers/auth.js
+++ course/projects/blog/blog-api/controllers/auth.js
@@ -4,6 +4,7 @@ const signup = async (req, res, next) => {
 	// http://localhost:2000/api/v1/auth/signup
 
 	try {
+		throw new Error("Test Error");
 		const { name, email, password, role } = req.body;
 
 		const newUser = new User({ name, email, password, role });
