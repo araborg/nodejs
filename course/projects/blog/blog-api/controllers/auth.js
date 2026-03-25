b37e72855e98c39258b84f2250877896a22cd85d
araborg <arababs2015@gmail.com>
2026-03-25 05:44:01 +0100

    blog_api: 30

--- course/projects/blog/blog-api/controllers/auth.js
+++ course/projects/blog/blog-api/controllers/auth.js
@@ -6,36 +6,7 @@ const signup = async (req, res, next) => {
 	// http://localhost:2000/api/v1/auth/signup
 
 	try {
-		// Testing Error:
-		// res.code = 400
-		// throw new Error("Test Error");
-
 		const { name, email, password, role } = req.body;
-
-		// // errors for the user fields
-		// if (!name) {
-		// 	res.code = 400;
-
-		// 	throw new Error("Name is required");
-		// }
-
-		// if (!email) {
-		// 	res.code = 400;
-
-		// 	throw new Error("Email is required");
-		// }
-
-		// if (!password) {
-		// 	res.code = 400;
-
-		// 	throw new Error("Password is required");
-		// }
-
-		// if (password.length < 6) {
-		// 	res.code = 400;
-
-		// 	throw new Error("Password should be 6 character long");
-		// }
 
 		const isEmailExist = await User.findOne({ email });
 
