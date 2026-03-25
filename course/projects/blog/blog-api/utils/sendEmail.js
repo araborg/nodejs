6e8bd492b46862f8f0ca2ad45a6fa04b5d543eb0
araborg <arababs2015@gmail.com>
2026-03-25 16:23:01 +0100

    blog_api: 52

--- course/projects/blog/blog-api/utils/sendEmail.js
+++ course/projects/blog/blog-api/utils/sendEmail.js
@@ -8,6 +8,16 @@ const sendEmail = async ({ emailTo, subject, code, content }) => {
 		// auth: { user: "hironse.96@gmail.com", pass: "" },
 		auth: { user: "arababs2015@gmail.com", pass: "" },
 	});
+
+	const message = {
+		to: emailTo,
+		subject: subject,
+		html: `
+            <div>
+                <h3>Use this below code to </h3>
+            </div>
+        `,
+	};
 };
 
 module.exports = sendEmail;
