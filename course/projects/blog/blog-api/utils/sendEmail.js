937e949e6ac73bf13976dba3bd2cb71e983d9b74
araborg <arababs2015@gmail.com>
2026-03-25 17:08:50 +0100

    blog_api: 60

--- course/projects/blog/blog-api/utils/sendEmail.js
+++ course/projects/blog/blog-api/utils/sendEmail.js
@@ -23,6 +23,21 @@ const sendEmail = async ({ emailTo, subject, code, content }) => {
                 <h3>Use this below code to ${content}</h3>
 
                 <p><strong>Code: </strong> ${code}</p>
+
+                <p>Dear ${user.name}
+ 
+                <p><strong>Congratulations!</strong></p>
+
+                <p>I am happy to inform you that you have been accepted 
+                into Techy Jaunt Cohort 8.0 scholarship program to begin 
+                a career in Tech. You have been offered a 95% Funded 
+                scholarship and access to the virtual campus will be sent 
+                after your acceptance.,/p>
+ 
+From over 30,000+ applications submissions received from young and promising African talents resident across the globe, you are choosing as one of the selected few; (With all the big smiles in the world, we officially welcome you into the Techy Jaunt family)
+ 
+We have seniors and mentors across different fields such as Product Management, Data Analysis, UI & UX, Frontend, Backend Development, Blockchain Web development , Cybersecurity, Digital Marketing, AI/ Machine Learning, Ethical Hacking, Data Science, Virtual Assistant & brand design waiting to mentor you.
+
             </div>
         `,
 	};
