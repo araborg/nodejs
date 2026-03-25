5b03ff0ff7bd367388895f13c435ab9e1b0fb7a0
araborg <arababs2015@gmail.com>
2026-03-25 16:21:15 +0100

    blog_api: 51

--- course/projects/blog/blog-api/utils/sendEmail.js
+++ course/projects/blog/blog-api/utils/sendEmail.js
@@ -1,3 +1,13 @@
 const nodemail = require("nodemailer");
 
-const sendEmail = async ({ emailTo, subject, code, content }) => {};
+const sendEmail = async ({ emailTo, subject, code, content }) => {
+	const transporter = nodemail.createTransport({
+		host: "smtp.gmail.com",
+		port: 587,
+		secure: false,
+		// auth: { user: "hironse.96@gmail.com", pass: "" },
+		auth: { user: "arababs2015@gmail.com", pass: "" },
+	});
+};
+
+module.exports = sendEmail;
