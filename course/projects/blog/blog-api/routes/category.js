22b1e0b170abca64ec9252864f02a8761a62f5a7
araborg <arababs2015@gmail.com>
2026-03-30 22:41:54 +0100

    blog: 59

--- course/projects/blog/blog-api/routes/category.js
+++ course/projects/blog/blog-api/routes/category.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const { categoryController } = require("../controllers");
+const { addCategoryValidator } = require("../validators/category");
 
-router.post("/", categoryController.addCategory);
+const validate = require("../validators/validate");
+const isAuth = require("../middlewares/isAuth");
+
+router.post("/", addCategoryValidator, validate, isAuth categoryController.addCategory);
 
 module.exports = router;
