3230fe3eb4d8de455d882443f5b58231822fe300
araborg <arababs2015@gmail.com>
2026-03-30 22:44:55 +0100

    blog: 60

--- course/projects/blog/blog-api/controllers/category.js
+++ course/projects/blog/blog-api/controllers/category.js
@@ -1,5 +1,15 @@
+const { Category } = require("../models");
+
 const addCategory = async (req, res, next) => {
 	try {
+		const { title, desc } = req.body;
+
+		const { _id } = req.user;
+
+		const isCategoryExist = await Category.findOne({ title });
+
+		if (isCategoryExist) {
+		}
 	} catch (error) {
 		next(error);
 	}
--- course/projects/blog/blog-api/routes/category.js
+++ course/projects/blog/blog-api/routes/category.js
@@ -7,6 +7,12 @@ const { addCategoryValidator } = require("../validators/category");
 const validate = require("../validators/validate");
 const isAuth = require("../middlewares/isAuth");
 
-router.post("/", addCategoryValidator, validate, isAuth categoryController.addCategory);
+router.post(
+	"/",
+	addCategoryValidator,
+	validate,
+	isAuth,
+	categoryController.addCategory,
+);
 
 module.exports = router;
