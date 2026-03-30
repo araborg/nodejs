8172fc65066b1c881e134bb237506e194e21e91f
araborg <arababs2015@gmail.com>
2026-03-30 16:44:30 +0100

    blog: 50

--- course/projects/blog/blog-api/models/Category.js
+++ course/projects/blog/blog-api/models/Category.js
@@ -1,3 +1,22 @@
 const mongoose = require("mongoose");
 
-const categorySchema = mongoose.Schema();
+const categorySchema = mongoose.Schema(
+	{
+		title: { type: String, required: true },
+
+		desc: String,
+
+		// d user here mimic dt in User schema
+		updatedBy: {
+			type: mongoose.Types.ObjectId,
+			ref: "user",
+			required: true,
+		},
+	},
+
+	{ timestamps: true },
+);
+
+const Category = mongoose.model("category", categorySchema);
+
+module.exports = Category;
