75f2926c12079be5bd6ee9bfbd8f70a647d9049f
araborg <arababs2015@gmail.com>
2026-03-30 15:45:02 +0100

    blog: 41

--- course/projects/blog/blog-api/validators/validateEmail.js
+++ course/projects/blog/blog-api/validators/validateEmail.js
@@ -1,3 +1,9 @@
 const validateEmail = (email) => {
-	email.match();
+	const result = email.match(
+		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+	);
+
+	return result;
 };
+
+module.exports = validateEmail;
