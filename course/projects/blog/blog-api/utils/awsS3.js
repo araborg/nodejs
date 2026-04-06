81e9543795105a621ab27b380437433377811df3
araborg <arababs2015@gmail.com>
2026-04-06 15:33:08 +0100

    node-blog; 14

--- course/projects/blog/blog-api/utils/awsS3.js
+++ course/projects/blog/blog-api/utils/awsS3.js
@@ -3,7 +3,9 @@ const {
 	awsRegion,
 	awsAccessKey,
 	awsSecretAccessKey,
+	awsBucketName,
 } = require("../config/keys");
+const generateCode = require("./generateCode");
 
 const client = new S3Client({
 	region: awsRegion,
@@ -15,7 +17,28 @@ const client = new S3Client({
 	},
 });
 
-const uploadFile = async ({ file, ext }) => {};
+const uploadFile = async ({ file, ext }) => {
+	// some_random_number.ext
+	const Key = `${generateCode(12)}_${Date.now()}${ext}`;
+
+	const params = {
+		Bucket: awsBucketName,
+
+		Body: file.buffer,
+		key: key,
+		ContentType: file.mimetype,
+	};
+
+	const command = new PutObjectCommand(params);
+
+	try {
+		await client.send(command);
+
+		return Key;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 module.exports = { uploadFile };
 
