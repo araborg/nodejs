77270fdd32bad862e41ecac32023d9876b9ce0f8
araborg <arababs2015@gmail.com>
2026-04-06 15:16:13 +0100

    node-blog; 9

--- course/projects/blog/blog-api/controllers/file.js
+++ course/projects/blog/blog-api/controllers/file.js
@@ -1,8 +1,29 @@
+const path = require("path");
+const { validateExtension } = require("../validators/file");
+
 const uploadFile = async (req, res, next) => {
 	// http://localhost:2000/api/v1/file/upload
 
 	try {
-		res.json({ ok: true });
+		// res.json({ ok: true });
+
+		const { file } = req;
+
+		if (!file) {
+			res.code = 400;
+
+			throw new Error("File is not selected");
+		}
+
+		const ext = path.extname(file.originalname);
+
+		const isValidExt = validateExtension(ext);
+
+		if (!isValidExt) {
+			res.code = 400;
+
+			throw new Error("Only .jpg or .jpeg or .png is allowed");
+		}
 	} catch (error) {
 		next(error);
 	}
