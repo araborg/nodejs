b4e05a4d35c32ab38268f55511f184d72fa04af7
araborg <arababs2015@gmail.com>
2026-04-06 15:16:49 +0100

    node-blog; 10

--- course/projects/blog/blog-api/controllers/file.js
+++ course/projects/blog/blog-api/controllers/file.js
@@ -5,8 +5,6 @@ const uploadFile = async (req, res, next) => {
 	// http://localhost:2000/api/v1/file/upload
 
 	try {
-		// res.json({ ok: true });
-
 		const { file } = req;
 
 		if (!file) {
@@ -24,6 +22,8 @@ const uploadFile = async (req, res, next) => {
 
 			throw new Error("Only .jpg or .jpeg or .png is allowed");
 		}
+
+		res.json({ ok: true });
 	} catch (error) {
 		next(error);
 	}
