f06c179b9815b8c4934ee19469725652164563fe
araborg <arababs2015@gmail.com>
2026-04-06 15:26:54 +0100

    node-blog; 13

--- course/projects/blog/blog-api/utils/awsS3.js
+++ course/projects/blog/blog-api/utils/awsS3.js
@@ -1,3 +1,20 @@
+const { PutObjectCommand, S3Client } = require("@aws-sdk/client-s3");
+const {
+	awsRegion,
+	awsAccessKey,
+	awsSecretAccessKey,
+} = require("../config/keys");
+
+const client = new S3Client({
+	region: awsRegion,
+
+	credentials: {
+		accessKeyId: awsAccessKey,
+
+		secretAccessKey: awsSecretAccessKey,
+	},
+});
+
 const uploadFile = async ({ file, ext }) => {};
 
 module.exports = { uploadFile };
