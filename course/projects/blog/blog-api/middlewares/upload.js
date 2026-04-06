c358036c7d3ec033aa0b6ba59f2e1427c7ab5fb0
araborg <arababs2015@gmail.com>
2026-04-06 15:05:55 +0100

    node-blog; 7

--- course/projects/blog/blog-api/middlewares/upload.js
+++ course/projects/blog/blog-api/middlewares/upload.js
@@ -3,5 +3,7 @@ const multer = require("multer");
 const upload = multer({
 	storage: multer.memoryStorage(),
 
-	limits: {},
+	limits: { fileSize: 1024 * 1024 * 50 },
 });
+
+module.exports = upload;
