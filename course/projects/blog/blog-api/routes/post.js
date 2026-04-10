552b061c6e4882ed2fca4cd2e753ece147a0456a
araborg <arababs2015@gmail.com>
2026-04-10 09:48:29 +0100

    blog_api: 16

--- course/projects/blog/blog-api/routes/post.js
+++ course/projects/blog/blog-api/routes/post.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const isAuth = require("../middlewares/isAuth");
 const { postController } = require("../controllers");
+const { addPostValidator } = require("../validators/post");
+const validate = require("../validators/validate");
 
-router.post("/", isAuth, postController.addPost);
+router.post("/", isAuth, addPostValidator, validate postController.addPost);
 
 module.exports = router;
