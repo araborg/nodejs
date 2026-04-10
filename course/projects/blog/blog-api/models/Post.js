9e9409bfa83952a9d2f16fbee8dc55fe968dfc70
araborg <arababs2015@gmail.com>
2026-04-10 09:25:48 +0100

    blog_api: 10

--- course/projects/blog/blog-api/models/Post.js
+++ course/projects/blog/blog-api/models/Post.js
@@ -1,3 +1,26 @@
 const mongoose = require("mongoose");
 
-const postSchema = mongoose.Schema({});
+const postSchema = mongoose.Schema(
+	{
+		title: { type: String, required: true },
+
+		desc: String,
+
+		file: { type: mongoose.Types.ObjectId, ref: "file" },
+
+		category: {
+			type: mongoose.Types.ObjectId,
+			ref: "category",
+			required: true,
+		},
+		updatedBy: {
+			type: mongoose.Types.ObjectId,
+			ref: "user",
+			required: true,
+		},
+	},
+
+	{ timestamps: true },
+);
+
+const Post = mongoose.model("post", postSchema);
