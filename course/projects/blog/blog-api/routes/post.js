765b40d74fa993480194dcb3fbdbeb60f1b032ff
araborg <arababs2015@gmail.com>
2026-04-10 09:40:13 +0100

    blog_api: 14

--- course/projects/blog/blog-api/routes/post.js
+++ course/projects/blog/blog-api/routes/post.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const isAuth = require("../middlewares/isAuth");
+const { postController } = require("../controllers");
+
+router.post("/", isAuth, postController.addPost);
 
 module.export = router;
